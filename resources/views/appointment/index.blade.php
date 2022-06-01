b2e386d39b471ee48781744c5a3d926778af0307
Barbara Bianco <barbarabiancov@gmail.com>
2022-06-01 17:48:20 -0300

    ajuste cale

--- resources/views/appointment/index.blade.php
+++ resources/views/appointment/index.blade.php
@@ -15,7 +15,7 @@
     <link href=' {{ asset('css/main.css') }}' rel='stylesheet' />
 
 <body>
-
+    
 
 
     <nav class="navbar navbar-expand-sm nav-menu" style="background: #fff">
@@ -32,7 +32,7 @@
                     </svg>
                 </li>
                 <li class="logo">
-                    <img src="https://ssl.gstatic.com/calendar/images/dynamiclogo_2020q4/calendar_<?php echo date('d'); ?>_2x.png"
+                    <img src="https://ssl.gstatic.com/calendar/images/dynamiclogo_2020q4/calendar_2_2x.png"
                         width="40" height="40" alt="">
                     <span style="margin-left:10px; font-weight:500; font-size:20px; #ccc">Agenda</span>
                 </li>
@@ -60,9 +60,15 @@
             <div class="col-1" style="float:left;">
                 <button class="botao-hoje" id="my-today-button">Hoje</button>
             </div>
-            <span class="m-1" style="margin-top:10px;">
-                <div class="prev" id="prev"><i class="fa-solid fa-angle-left"></i></div>
-                <div class="next" id="next"><i class="fa-solid fa-angle-right"></i></div>
+            
+            <div class="nextprev-header">
+                <div class="prev" id="prev">
+                    <i class="fa-solid fa-angle-left"></i>
+                </div>
+                <div class="next" id="next">
+                    <i class="fa-solid fa-angle-right"></i>
+                </div>
+            </div>
 
                 <span class="data-header fc-header-title my-2" style="font-size:20px; margin-left:20px;"></span>
             </span>
@@ -71,9 +77,11 @@
         <div class="col-3 right-header-nav">
             <i class="fa fa-search buscar_pessoas_agenda"></i>
 
-            <i class="fa-solid fa-circle-question interrogacao-icon"></i>
+            <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" class="interrogacao-icon">
+                <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"></path></svg>
 
-            <i class="fa fa-cog m-1" style="font-size:30px; "></i>
+            <svg width="24" height="24" viewBox="0 0 24 24" focusable="false" class="config-icon">
+                <path d="M13.85 22.25h-3.7c-.74 0-1.36-.54-1.45-1.27l-.27-1.89c-.27-.14-.53-.29-.79-.46l-1.8.72c-.7.26-1.47-.03-1.81-.65L2.2 15.53c-.35-.66-.2-1.44.36-1.88l1.53-1.19c-.01-.15-.02-.3-.02-.46 0-.15.01-.31.02-.46l-1.52-1.19c-.59-.45-.74-1.26-.37-1.88l1.85-3.19c.34-.62 1.11-.9 1.79-.63l1.81.73c.26-.17.52-.32.78-.46l.27-1.91c.09-.7.71-1.25 1.44-1.25h3.7c.74 0 1.36.54 1.45 1.27l.27 1.89c.27.14.53.29.79.46l1.8-.72c.71-.26 1.48.03 1.82.65l1.84 3.18c.36.66.2 1.44-.36 1.88l-1.52 1.19c.01.15.02.3.02.46s-.01.31-.02.46l1.52 1.19c.56.45.72 1.23.37 1.86l-1.86 3.22c-.34.62-1.11.9-1.8.63l-1.8-.72c-.26.17-.52.32-.78.46l-.27 1.91c-.1.68-.72 1.22-1.46 1.22zm-3.23-2h2.76l.37-2.55.53-.22c.44-.18.88-.44 1.34-.78l.45-.34 2.38.96 1.38-2.4-2.03-1.58.07-.56c.03-.26.06-.51.06-.78s-.03-.53-.06-.78l-.07-.56 2.03-1.58-1.39-2.4-2.39.96-.45-.35c-.42-.32-.87-.58-1.33-.77l-.52-.22-.37-2.55h-2.76l-.37 2.55-.53.21c-.44.19-.88.44-1.34.79l-.45.33-2.38-.95-1.39 2.39 2.03 1.58-.07.56a7 7 0 0 0-.06.79c0 .26.02.53.06.78l.07.56-2.03 1.58 1.38 2.4 2.39-.96.45.35c.43.33.86.58 1.33.77l.53.22.38 2.55z"></path><circle cx="12" cy="12" r="3.5"></circle></svg>
 
             <a href="#" title="" class="btn btn-light botao-header-datas">
                 Mês <span class="icon">
@@ -86,33 +94,38 @@
                     <li class="semana" id="semana">Semana</li>
                     <li class="mes" id="mes">Mês</li>
                     <li class="ano" id="ano">Ano</li>
-                    <li class="programacao" id="programacao">Programação</li>
-                    <li class="4dias" id="4dias">4 Dias</li>
+                    {{-- <li class="programacao" id="programacao">Programação</li>
+                    <li class="4dias" id="4dias">4 Dias</li> --}}
                 </ul>
 
             </div>
         </div>
-
-
-
     </nav>
 
 
     <div class="container-fluid p-0">
-        <div class="col-2  sider-bar" style="float:left;  border-right:1px solid #ccc;;">
+        <div class="col-2  sider-bar" style="float:left;  border-top:1px solid #ccc;">
 
 
-            <div class="col-sm-12" style="height:200px;">
-                <button class="btn btn-success bt-criar-evento">Criar
-                    <span class="icon">
+            <div class="col-sm-12 mb-3">
+                <button class="btn  bt-criar-evento">
+                    <svg width="36" height="36" viewBox="0 0 36 36">
+                        <path fill="#34A853" d="M16 16v14h4V20z"></path>
+                            <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+                            <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+                            <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
+                            <path fill="none" d="M0 0h36v36H0z"></path>
+                        </svg>
+                     <span class="bt-criar-evento-text" >Criar</span>   
+                    <span class="icon icon-criar-evento">
                         <i class="fa-solid fa-caret-down"></i>
                     </span>
                 </button>
             </div>
-            <div class="header-calendario2" style="display:flex;margin-bottom:10px;">
-                <span class="data_calendario2">Fevereiro de 2022</span>
-                <div class="seta-calendario2" style="width:50px;  margin-left:48%;">
-                    <div class="prev-calendario2" id="prev-calendario2" style="float:left; margin-right:20px;">
+            <div class="header-calendario2" style="">
+                <span class="data_calendario2"></span>
+                <div class="seta-calendario2">
+                    <div class="prev-calendario2" id="prev-calendario2">
                         <i class="fa-solid fa-angle-left"></i>
                     </div>
                     <div class="next-calendario2" id="next-calendario2">
@@ -120,8 +133,7 @@
                    </div>
                 </div>
             </div>
-            <div id="calendario2"
-                style="padding:1px; border:0; min-height:430px; width:95%; color:red; font-size:10px;">
+            <div id="calendario2">
 
 
             </div>
@@ -186,22 +198,31 @@
                         </ul>
 
                     </div>
-
-
-
                 </div>
-            </div>
-            <div class="col-12 my-5" style=" padding:10px; float:left; ">
-
-            </div>
+              </div>
+            <div class="col-12 my-5" style=" padding:10px; float:left;"></div>
         </div>
-        <div class="col-10" id="calendar" style="float:left;  border:1px solid #ccc;;">
-
-
-        </div>
+        <div class="col-10" id="calendar" style="float:left;"></div>
     </div>
 
-
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default bt-close-modal" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
 
 </body>
 
@@ -235,7 +256,23 @@
 
 
         $('.icons-menu-bottom').click(function() {
-            $('.sider-bar').toggle();
+            $('.sider-bar').toggle();  
+            $('.fc-daygrid-bod').css('width','100%');         
+            $('#calendar').addClass('col-10');
+            $('#calendar').removeClass('col-12');
+        
+            if($('.sider-bar').css('display') == 'none'){
+                $('#calendar').removeClass('col-10');
+                $('#calendar').addClass('col-12');
+                $('.fc-col-header').css('width','100%');
+                $('.fc-daygrid-body').css('width','100%');
+                $('.fc-scrollgrid-sync-table').css('width','100%');            
+            }
+
+            calendar.render();
+
+           
+           
         })
 
         $('.component_datas').click(function() {
@@ -258,26 +295,139 @@
             $('.opcoesOutrasAgendas').toggle('fast');
         })
 
+          $('.bt-close-modal').click(function() {
+            $('.modal').modal('hide');
+        })
+
 
     })
    
 
     document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
-        var calendarE2 = document.getElementById('calendar2');
+      
         var calendar = new FullCalendar.Calendar(calendarEl, {
             editable:true,
             lang: 'pt-br',
             headerToolbar: {
                 left: 'prev,next today',
                 center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth, month'
             },
             initialDate: '2022-05-31',
             locale:'pt-br',
 
             navLinks: true, // can click day/week names to navigate views
             businessHours: true, // display business hours
+            editable: true,
+            selectable: true,
+            events: [
+            {
+            id: 1,
+            title: "Meeting",
+            start: "2022-06-01T14:30:00",
+            extendedProps: {
+                status: "done"
+            }          
+                 } 
+          
+  ],
+            selectable:true,
+            selectHelper:true,
+            select: function(start, end, allDay)
+            {
+              $('.modal').modal('show');
+            }
+        });
+
+        
+
+        calendar.render();
+
+        $('.fc-header-toolbar').hide();
+        mudarNomesDiasThCalendario()
+        $('.data-header').text($('.fc-toolbar-title').text())
+        document.getElementById('my-today-button').addEventListener('click', function() {
+            calendar.today();
+            calendar2.today();
+            $('.data-header').text($('.fc-toolbar-title').text())
+           
+
+        });
+
+
+        document.getElementById('dia').addEventListener('click', function() {       
+            calendar.changeView('timeGridDay');
+
+        });
+
+        document.getElementById('semana').addEventListener('click', function() {
+            calendar.changeView('timeGridWeek');
+
+
+        });
+
+        document.getElementById('mes').addEventListener('click', function() {
+            calendar.changeView('dayGridMonth');        
+         
+        });
+
+        document.getElementById('ano').addEventListener('click', function() {
+            calendar.changeView('yearGridMonth');        
+         
+        });
+
+        document.getElementById('next').addEventListener('click', function() {
+            calendar.next();
+            $('.data-header').text(calendar.currentData.viewTitle)    
+          
+        });
+
+        document.getElementById('prev').addEventListener('click', function() {
+            calendar.prev();
+            $('.data-header').text(calendar.currentData.viewTitle)          
+
+        });
+
+
+      
+
+
+        function mudarNomesDiasThCalendario() {
+            $(diasCalendario1('sun')).text('Dom')
+            $(diasCalendario1('mon')).text('Seg')
+            $(diasCalendario1('tue')).text('Ter')
+            $(diasCalendario1('wed')).text('Qua')
+            $(diasCalendario1('thu')).text('Qui')
+            $(diasCalendario1('fri')).text('Sex')
+            $(diasCalendario1('sat')).text('Sab')
+            $(diasCalendario2('sun')).text('D')
+            $(diasCalendario2('mon')).text('S')
+            $(diasCalendario2('tue')).text('T')
+            $(diasCalendario2('wed')).text('Q')
+            $(diasCalendario2('thu')).text('Q')
+            $(diasCalendario2('fri')).text('S')
+            $(diasCalendario2('sat')).text('S')
+        }
+
+    });
+
+
+
+    var calendarE2 = document.getElementById('calendario2');
+    var calendar2 = new FullCalendar.Calendar(calendarE2, {      
+        aspectRatio: 20,
+            editable:true,
+            isRTL: false,
+            lang: 'pt-br',      
+            headerToolbar: {
+                left: 'prev,next today',
+                center: '',
+                right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+            },
+            locale:'pt-br',           
+            navLinks: false, // can click day/week names to navigate views
+            businessHours: false, // display business hours
             editable: true,
             selectable: true,
             events: [],
@@ -289,82 +439,32 @@
             }
         });
 
+        calendar2.render()
+      
         
+        $('.data_calendario2').text(calendar2.currentData.viewTitle)
+        $('.fc-scroller').css('overflow','hidden')
 
+        document.getElementById('prev-calendario2').addEventListener('click', function() {         
+            calendar2.prev();
+            $('.data_calendario2').text(calendar2.currentData.viewTitle)
+            
 
-
-        calendar.render();
-        $('.fc-header-toolbar').hide();
-        changeName()
-        $('.data-header').text($('.fc-toolbar-title').text())
-        document.getElementById('my-today-button').addEventListener('click', function() {
-            calendar.today();
-            $('.data-header').text($('.fc-toolbar-title').text())
-            component()
+        });
+   
+        document.getElementById('next-calendario2').addEventListener('click', function() {
+            calendar2.prev();
+            $('.data_calendario2').text(calendar2.currentData.viewTitle)                   
 
         });
 
 
-        document.getElementById('dia').addEventListener('click', function() {
-            component()
 
-
-        });
-
-        document.getElementById('semana').addEventListener('click', function() {
-            component()
-
-
-        });
-
-        document.getElementById('mes').addEventListener('click', function() {
-            component()
-
-
-        });
-
-        document.getElementById('next').addEventListener('click', function() {
-            calendar.next();
-            $('.data-header').text($('.fc-toolbar-title').text())
-            component()
-        });
-
-        document.getElementById('prev').addEventListener('click', function() {
-            calendar.prev();
-            $('.data-header').text($('.fc-toolbar-title').text())
-            component()
-
-        });
-
-
-        function component() {
-            $('.dia').html($('.fc-timeGridDay-button'))
-            $('.semana').html($('.fc-timeGridWeek-button'))
-            $('.mes').html($('.fc-dayGridMonth-button'))
-            $('.4dia').html($('.fc-listMonth-button'))
+        function diasCalendario2(dia){
+           return '#calendario2 > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-'+dia+' > div > a';
         }
-
-
-        function changeName() {
-            $('#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-sun > div > a')
-                .text('Domingo')
-            $('#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-mon > div > a')
-                .text('Segunda')
-            $('#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-tue > div > a')
-                .text('Terça')
-            $('#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-wed > div > a')
-                .text('Quarta')
-            $('#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-thu > div > a')
-                .text('Quin')
-            $('#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-fri > div > a')
-                .text('Sex')
-            $('#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-sat > div > a')
-                .text('Sabado')
-        }
-
-    });
-
-
-
+        function diasCalendario1(dia){
+           return '#calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > table > thead > tr > th.fc-col-header-cell.fc-day.fc-day-'+dia+' > div > a';
+        }         
    
 </script>
